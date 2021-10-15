88b9ef78e81abdb69e6270df8e55bba1ab2b1562
Roberto Chen <roberto.chenf@gmail.com>
2021-10-14 21:31:41 -0500

    initial

---
+++ calendario_feriados/holidays.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import json
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    service = build("calendar", "v3", credentials=creds)
+
+    # Get Panama Holidays
+    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+
+    YEAR = datetime.datetime.now().year
+    startDate = datetime.datetime(YEAR, 1, 1).isoformat() + "Z"
+    endDate = datetime.datetime(YEAR, 12, 31).isoformat() + "Z"
+
+    eventsResult = (
+        service.events()
+        .list(
+            calendarId="en.pa#holiday@group.v.calendar.google.com",
+            timeMin=startDate,
+            timeMax=endDate,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+
+    # Save Holidays to JSON file
+    data = json.dumps(eventsResult)
+    with open("holidays.json", "w", encoding="utf8") as json_file:
+        json_file.write(data)
+
+
+if __name__ == "__main__":
+    main()
