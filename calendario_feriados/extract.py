5ad217082bcd85035c135bc265fa366afa37a258
Roberto Chen <roberto.chenf@gmail.com>
2021-10-14 21:57:44 -0500

    format code struct

--- calendario_feriados/extract.py
+++ calendario_feriados/extract.py
@@ -1,11 +1,11 @@
 import json
 
 # Read JSON file
-with open('holidays.json','r',encoding="utf8") as json_file:
+with open("holidays.json", "r", encoding="utf8") as json_file:
     data = json.load(json_file)
 
 # Extract date and save to txt
-with open('dates.txt','w',encoding='utf8') as txt_file:
-    for item in data['items']:
-        txt_file.write(item['start']['date'] + '\n')
-        print(item['start']['date'] + " " + item['summary'])
+with open("dates.txt", "w", encoding="utf8") as txt_file:
+    for item in data["items"]:
+        txt_file.write(item["start"]["date"] + "\n")
+        print(item["start"]["date"] + " " + item["summary"])
